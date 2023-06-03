c924a5bfba8937812c7ffd5c1fda6385ea004396
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-03 01:29:31 -0700

    Added read_bytes comment

--- html_file/__init__.py
+++ html_file/__init__.py
@@ -7,10 +7,16 @@ from extended_path import ExtendedPath
 class HTMLFile(ExtendedPath):
     def parsed(self) -> ExtendedBeautifulSoup:
         """Read and parse an html file"""
+
+        # I don't know of any reason why you would want to use read_text instead of read_bytes for BeautifulSoup
+        # For now this function will always just use read_bytes
         return ExtendedBeautifulSoup(self.read_bytes(), "lxml")
 
     def parsed_cached(self, reload: bool = False) -> ExtendedBeautifulSoup:
         """Read and parse an html file with caching"""
+
+        # I don't know of any reason why you would want to use read_text instead of read_bytes for BeautifulSoup
+        # For now this function will always just use read_bytes
         if not hasattr(self, "cached_parsed_html") or reload:
             self.cached_parsed_html = ExtendedBeautifulSoup(self.read_bytes_cached(reload), "lxml")
 
