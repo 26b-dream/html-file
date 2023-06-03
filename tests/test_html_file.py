26ae0a83d8ea09aeadf5a3265ac74bc4dd8c7344
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-03 01:27:53 -0700

    Changed temporary file names to match test name

--- tests/test_html_file.py
+++ tests/test_html_file.py
@@ -4,14 +4,14 @@ from html_file import HTMLFile
 
 
 def test_parsed():
-    file = HTMLFile("tests/input.html")
+    file = HTMLFile("tests/test_parsed.html")
     file.write("<title>Example</title>")
     assert file.parsed().strict_select_one("title").text == "Example"
     file.delete()
 
 
 def test_parsed_cached_changed_file():
-    file = HTMLFile("tests/input.html")
+    file = HTMLFile("tests/test_parsed_cached_changed_file.html")
     file.write("<title>Example</title>")
     file.parsed_cached()
     file.write("")
@@ -20,7 +20,7 @@ def test_parsed_cached_changed_file():
 
 
 def test_parsed_cached_updating_cache():
-    file = HTMLFile("tests/input.html")
+    file = HTMLFile("tests/test_parsed_cached_updating_cache.html")
     file.write("<title>Example</title>")
     file.parsed_cached()
     file.write("<title>Title</title>")
