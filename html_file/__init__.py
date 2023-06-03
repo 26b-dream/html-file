c09c9dbb7fed37f912b8672211257d03dbf17a4f
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-03 01:24:50 -0700

    Added caching of parsed values

--- html_file/__init__.py
+++ html_file/__init__.py
@@ -5,6 +5,13 @@ from extended_path import ExtendedPath
 
 
 class HTMLFile(ExtendedPath):
-    def parsed_html(self) -> ExtendedBeautifulSoup:
+    def parsed(self) -> ExtendedBeautifulSoup:
         """Read and parse an html file with caching"""
         return ExtendedBeautifulSoup(self.read_bytes(), "lxml")
+
+    def parsed_cached(self, reload: bool = False) -> ExtendedBeautifulSoup:
+        """Read and parse an html file with caching"""
+        if not hasattr(self, "cached_parsed_html") or reload:
+            self.cached_parsed_html = ExtendedBeautifulSoup(self.read_bytes_cached(reload), "lxml")
+
+        return self.cached_parsed_html
--- tests/test_html_file.py
+++ tests/test_html_file.py
@@ -1,8 +1,28 @@
+from extended_path import ExtendedPath
+
 from html_file import HTMLFile
 
 
-def test_strict_select_one():
-    """Test the HTMLFile class"""
-    html_file = HTMLFile("tests/input.html")
+def test_parsed():
+    file = HTMLFile("tests/input.html")
+    file.write("<title>Example</title>")
+    assert file.parsed().strict_select_one("title").text == "Example"
+    file.delete()
 
-    assert html_file.parsed_html().strict_select_one("title").text == "Example"
+
+def test_parsed_cached_changed_file():
+    file = HTMLFile("tests/input.html")
+    file.write("<title>Example</title>")
+    file.parsed_cached()
+    file.write("")
+    assert file.parsed_cached().strict_select_one("title").text == "Example"
+    file.delete()
+
+
+def test_parsed_cached_updating_cache():
+    file = HTMLFile("tests/input.html")
+    file.write("<title>Example</title>")
+    file.parsed_cached()
+    file.write("<title>Title</title>")
+    assert file.parsed_cached(True).strict_select_one("title").text == "Title"
+    file.delete()
